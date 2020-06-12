d2e2a878c6bf4414b396b3ea24e59acf57247d6f
Kyouma-BAT <kyouma.bat@gmail.com>
2020-06-12 13:49:21 +0600

    saf

--- ChatSystem/ChatSystem/settings.py
+++ ChatSystem/ChatSystem/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +27,7 @@ SECRET_KEY = '*0j15k*oyr!6&_mag7e^q%7+d&ssj=l3^m**ev2b(7t*^c$^_('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['5.76.194.93','192.168.3.102']
+ALLOWED_HOSTS = ['5.76.194.93','192.168.3.100']
 
 
 # Application definition
