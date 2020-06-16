997cd3bd1670cc09fabca5511a040fc25e8d252e
Kyouma-BAT <kyouma.bat@gmail.com>
2020-06-16 19:49:54 +0600

    Another legendary push

--- ChatSystem/chat/forms.py
+++ ChatSystem/chat/forms.py
@@ -2,4 +2,7 @@ from django import forms
 
 
 class NameForm(forms.Form):
-    your_name = forms.CharField(label = '', max_length = 100)
+    your_name = forms.CharField(label = '', max_length = 100, widget = forms.TextInput(attrs = {
+        'type':"text",
+        'placeholder':"Enter name here",
+        'id':"nameForm"}))
